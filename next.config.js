c287296b62a3bdbd623da7da93de3b5941278244
mkingstonsqr <mkingston@squareup.com>
2025-09-23 16:50:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,11 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
 }
 
 module.exports = nextConfig
